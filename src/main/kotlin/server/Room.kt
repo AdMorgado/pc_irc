623ac02e3a933e965b4ee84a268758b96c36382e
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-06-16 15:34:47 +0100

    Implement room class

--- src/main/kotlin/server/Room.kt
+++ src/main/kotlin/server/Room.kt
@@ -1,8 +1,31 @@
 package server;
 
+import java.util.concurrent.ConcurrentHashMap
+
 
 class Room {
 
+    private val connectedSessions = ConcurrentHashMap<Int, Session>()
+
+    val roaster : List<Session>
+        get() = connectedSessions.values.toList()
+
+    suspend fun join(session : Session)
+    {
+        connectedSessions[session.id] = session;
+    }
+
+    suspend fun leave(session : Session)
+    {
+        connectedSessions.remove(session.id);
+    }
+
+    suspend fun sendMessage(msg : Message)
+    {
+        roaster.forEach {
+            it.send(msg);
+        }
+    }
 }
 
 
